aa22f3b147a22a2b33f264bbc3834f1907fc932e
Crowdbotics <team@crowdbotics.com>
2022-07-26 16:41:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "luas_hvac_36426",
+  url: "https://luas_hvac_36426.botics.co",
+  api: "https://luas_hvac_36426.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
